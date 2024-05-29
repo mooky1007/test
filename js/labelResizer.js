82aacc792053cbd51540d4c771de799cf09c5697
mooky <mooky1007@naver.com>
2024-05-29 15:48:03 +0900

    update : lang change case & title edit

--- js/labelResizer.js
+++ js/labelResizer.js
@@ -1,8 +1,10 @@
 class LabelResizer {
     constructor(el) {
         this.el = el;
+        this.timer = null;
+
         this.observe();
-        window.addEventListener('resize', () => this.render());
+        window.addEventListener('resize', this.debounce(() => this.render(), 100));
     }
 
     collectData() {
@@ -13,7 +15,7 @@ class LabelResizer {
 
             cols.forEach((col, idx) => {
                 const colspan = col.className.match(/col-(\d+)/)[1];
-                const labelEl = col.querySelector('label');
+                const labelEl = col.querySelector('.label_cell');
                 const colIdx = idx === 0 ? 0 : +cols[idx - 1].getAttribute('col-idx') + (+cols[idx - 1].className.match(/col-(\d+)/)[1] || 1);
                 col.setAttribute('col-idx', colIdx);
 
@@ -25,6 +27,7 @@ class LabelResizer {
     }
 
     observe() {
+        // 동적으로 라벨내용이 변경되는 일이 없다면 해당 함수제거
         const observer = new MutationObserver(() => this.render());
         observer.observe(this.el, {
             childList: true,
@@ -32,7 +35,15 @@ class LabelResizer {
             characterDataOldValue: true,
         });
 
-        this.render()
+        this.render();
+    }
+
+    debounce(fn, delay) {
+        let timer = null;
+        return (...args) => {
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(() => fn(...args), delay);
+        };
     }
 
     render() {
